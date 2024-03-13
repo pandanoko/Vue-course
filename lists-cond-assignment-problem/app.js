9e4164053e0d2a09bdfcaa9927b43c7c2c566372
Sara Russo <pandessa@gmail.com>
2024-03-13 12:52:38 +0100

    assignment lists

---
+++ lists-cond-assignment-problem/app.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            goalInputValue: "",
+            goals: [],
+            isVisible: true,
+        }
+    },
+    watch: {
+        goals() {
+
+        }
+    },
+    methods: {
+        addGoal() {
+            if (this.goalInputValue) {
+                this.goals.push(this.goalInputValue);
+                this.goalInputValue = ""
+            } 
+        },
+        removeGoal(idx){
+            this.goals.splice(idx, 1)
+        },
+        toggleVisibility() {
+            this.isVisible = !this.isVisible;
+        }
+    }
+})
+
+app.mount("#assignment");
